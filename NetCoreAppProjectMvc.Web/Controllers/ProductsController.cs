a307200bcca346fda8685a82382ac717c3b017be
hzeynep55 <83821699+hzeynep55@users.noreply.github.com>
2023-11-20 22:45:23 +0300

    more properties

--- NetCoreAppProjectMvc.Web/Controllers/ProductsController.cs
+++ NetCoreAppProjectMvc.Web/Controllers/ProductsController.cs
@@ -1,26 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreAppProjectMvc.Web.Models;
 using NetCoreAppProjectMvc.Web.Repository;
 
 namespace NetCoreAppProjectMvc.Web.Controllers
 {
 	public class ProductsController : Controller
 	{
+		private AppDbContext _context;
 		private readonly ProductRepository _productRepository;
-		public ProductsController()
+		public ProductsController(AppDbContext context)
 		{
+			_context=context;
 			_productRepository = new ProductRepository();
 
 			
 		}
 		public IActionResult Index()
 		{
-			var products = _productRepository.GetAll();
+			var products = _context.Products.ToList();
 			return View(products);
 		}
 
 		public IActionResult Remove(int id)
 		{
-			_productRepository.Remove(id);
+			var product=_context.Products.Find(id);
+			_context.Products.Remove(product);
+			_context.SaveChanges();
+
 			return RedirectToAction("Index");
 		}
 
