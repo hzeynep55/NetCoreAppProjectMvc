0dd0167bee9b5510cad07fc5855402775ebe036f
hzeynep55 <83821699+hzeynep55@users.noreply.github.com>
2023-12-11 23:39:18 +0300

    Requieds

--- NetCoreAppProjectMvc.Web/ViewModels/ProductViewModel.cs
+++ NetCoreAppProjectMvc.Web/ViewModels/ProductViewModel.cs
@@ -7,12 +7,15 @@ namespace NetCoreAppProjectMvc.Web.ViewModels
         public int Id { get; set; }
 
         [Required(ErrorMessage ="İsim alanı boş bırakılamaz.")]
+        [StringLength(50, ErrorMessage ="İsim alanına en fazla 50 karakter girilebilir.")]
         public string? Name { get; set; }
 
         [Required(ErrorMessage = "Fiyat alanı boş bırakılamaz.")]
+        [Range(1, 1000, ErrorMessage = "1-1000 arasında değer giriniz.")]
         public decimal? Price { get; set; }
 
         [Required(ErrorMessage = "Stok alanı boş bırakılamaz.")]
+        [Range(1,200, ErrorMessage = "1-200 arasında değer giriniz.")]
         public int? Stock { get; set; }
 
         [Required(ErrorMessage = "Açıklama alanı boş bırakılamaz.")]
